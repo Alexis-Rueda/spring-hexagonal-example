19aba549f1d5ecaca359a59d750cd22f88371fb3
alexis.rueda <alexisrueda34@gmail.com>
2026-07-07 23:05:00 -0600

    First commit

---
+++ src/main/java/com/rueda/todo_hexagonal/TodoHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.rueda.todo_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rueda/todo_hexagonal/TodoHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rueda.todo_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
